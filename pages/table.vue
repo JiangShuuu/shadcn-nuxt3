a24b23cd4d9c3dfedaee8cd6bba7047ce7ddad4f
JiangShuuu <globelex65@gmail.com>
2024-01-23 14:57:36 +0800

    add table

--- pages/table.vue
+++ pages/table.vue
@@ -7,14 +7,18 @@ const columns = [
   {
     key: "name",
     label: "Name",
+    sortable: true,
   },
   {
     key: "title",
     label: "Title",
+    sortable: true,
   },
   {
     key: "email",
     label: "Email",
+    sortable: true,
+    direction: "desc",
   },
   {
     key: "role",
@@ -67,23 +71,75 @@ const people = [
     email: "floyd.miles@example.com",
     role: "Member",
   },
+  {
+    id: 7,
+    name: "123 Francis",
+    title: "Copywriter",
+    email: "321.francis@example.com",
+    role: "Admin",
+  },
+  {
+    id: 8,
+    name: "qwe Krasner",
+    title: "Senior Designer",
+    email: "ewq.krasner@example.com",
+    role: "Owner",
+  },
+  {
+    id: 9,
+    name: "zxc Miles",
+    title: "Principal Designer",
+    email: "ewq.miles@example.com",
+    role: "Member",
+  },
 ];
+
+const q = ref("");
+
+const filteredRows = computed(() => {
+  if (!q.value) {
+    return people.slice((page.value - 1) * pageCount, page.value * pageCount);
+  }
+
+  return people.filter((person) => {
+    return Object.values(person).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase());
+    });
+  });
+});
+
+const page = ref(1);
+const pageCount = 5;
+const selected = ref([people[1]])
 </script>
 
 <template>
   <div class="w-full h-screen flex items-center justify-center">
     <div>
       <div
-        class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700"
+        class="flex px-3 space-x-3 py-3.5 border-b border-gray-200 dark:border-gray-700"
       >
-        <USelectMenu
-          v-model="selectedColumns"
-          :options="columns"
-          multiple
-          placeholder="Columns"
+        <UInput v-model="q" placeholder="Filter people..." />
+
+        <ClientOnly>
+          <USelectMenu
+            v-model="selectedColumns"
+            :options="columns"
+            multiple
+            placeholder="Columns"
+          />
+        </ClientOnly>
+      </div>
+      <UTable v-model="selected" :columns="selectedColumns" :rows="filteredRows" />
+      <div
+        class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700"
+      >
+        <UPagination
+          v-model="page"
+          :page-count="pageCount"
+          :total="people.length"
         />
       </div>
-      <UTable :columns="selectedColumns" :rows="people" />
     </div>
   </div>
 </template>
